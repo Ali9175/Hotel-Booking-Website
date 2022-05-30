5742ec681ac609bbc684b29ac7e536281b57f7d1
Ali9175 <alism9175@gmail.com>
2022-05-30 12:08:02 +0300

    v2

---
+++ contact.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="eng">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equive="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=divice-width, initial-scale=1.0">
+    <title>Victory Hotel-CONTACT </title>
+    <?php require('inc/links.php'); ?>
+
+</head>
+
+<body class="bg-light">
+
+    <?php require('inc/header.php'); ?>
+
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">CONTACT US</h2>
+        <div class="h-line bg-dark"> </div>
+        <p class="text-center mt-5">
+            bla bla ba bla bla ba bla bla ba bla bla ba bla bla ba
+            bla bla ba bla bla ba bla bla ba bla bla ba bla bla ba
+            bla bla ba bla bla ba bla <br> bla ba bla bla ba bla bla ba
+            bla bla ba bla bla ba bla bla ba bla bla ba bla bla ba
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 mb-5 px-4">
+
+                <div class="bg-white rounded shadow p-4">
+                    <iframe class="w-100 rounded mb-4" height="320px"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52365.032138845025!2d35.847671391337855!3d34.886003928968826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15217e77890fb9a3%3A0xa072a491096e24b!2zVGFydHVz4oCOLCBTeXJpYQ!5e0!3m2!1sen!2s!4v1653727697388!5m2!1sen!2s"
+                        loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                    <h5>Address</h5>
+                    <a href="https://goo.gl/maps/wTKYWu82n9GtE4Cu7" target="_blank"
+                        class="d-inline-block text-decoration-none text-dark mb-2">
+                        <i class="bi bi-geo-alt-fill"></i> XYZ, Syria, Tartous
+                    </a>
+
+                    <h5 class="mt-4">Call us</h5>
+                    <a href="tel: +963996522488" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i>+963996522488
+                    </a>
+                    <br>
+                    <a href="tel: +963996522488" class="d-inline-block text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i>+963996522488
+                    </a>
+
+                    <h5 class="mt-4">Email</h5>
+                    <a href="mailto: ask.Victory@gmail.com" class="d-inline-block text-decoration-none text-dark">
+                        <i class="bi bi-envelope-fill"></i> ask.Victory@gmail.com
+                    </a>
+
+                    <h5 class="mt-4">Follow us</h5>
+                    <a href="#" class="d-inline-block mb-3 text-dark fs-5 me-2">
+                        <i class="bi bi-twitter me-1"></i>
+                    </a>
+                    <a href="#" class="d-inline-block mb-3 text-dark fs-5 me-2">
+                        <i class="bi bi-facebook me-1"></i>
+                    </a>
+                    <a href="#" class="d-inline-block text-dark fs-5">
+                        <i class="bi bi-instagram me-1"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 px-4">
+                <div class="bg-white rounded shadow p-4">
+                    <form>
+                        <h5>Send a message</h5>
+                        <div class="mb-3">
+                            <label class="form-label" style="font-weight: 500;">Name</label>
+                            <input type="text" class="form-control shadow-none">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" style="font-weight: 500;">Email</label>
+                            <input type="eamil" class="form-control shadow-none">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" style="font-weight: 500;">Subject</label>
+                            <input type="text" class="form-control shadow-none">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" style="font-weight: 500;">Message</label>
+                            <textarea class="form-control" rows="5" style="resize: none;"></textarea>
+                        </div>
+                        <button type="submit" class="btn text-dark custom-bg mt-3">SEND</button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <?php require('inc/footer.php'); ?>
+
+
+
+</html>
--- index.php
+++ index.php
@@ -138,57 +138,13 @@
                                 Room heater
                             </span>
                         </div>
-                        <div class="rating mb-4">
-                            <h6 class="mb-1">Rating</h6>
-                            <span class="badge rounded-pill bg-light">
-                                <i class="bi bi-star-fill text-warning"></i>
-                                <i class="bi bi-star-fill text-warning"></i>
-                                <i class="bi bi-star-fill text-warning"></i>
-                                <i class="bi bi-star-fill text-warning"></i>
-                                <i class="bi bi-star-fill text-warning"></i>
-                            </span>
-                        </div>
-                        <div class="d-flex justify-content-evenly mb-2">
-                            <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
-                            <a href="#" class="btn btn-sm btn-outline-dark shadow-none">More details</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 my-3">
-                <div class="card border-0 shadow" style="max-width: 350px; margin:auto;">
-                    <img src="images/rooms/1.jpg" class="card-img-top">
-                    <div class="card-body">
-                        <h5>Simple Room Name</h5>
-                        <h6 class="mb-4">20$ per night</h6>
-                        <div class="features mb-4">
-                            <h6 class="mb-1">Features</h6>
+                        <div class="guests mb-4">
+                            <h6 class="mb-1">Guests</h6>
                             <span class="badge bg-light text-dark text-wrap">
-                                2 Rooms
+                                5 Adults
                             </span>
                             <span class="badge bg-light text-dark text-wrap">
-                                1 Bathroom
-                            </span>
-                            <span class="badge bg-light text-dark text-wrap">
-                                1 Balcony
-                            </span>
-                            <span class="badge bg-light text-dark text-wrap">
-                                3 Sofa
-                            </span>
-                        </div>
-                        <div class="facilities mb-4">
-                            <h6 class="mb-1">Facilities</h6>
-                            <span class="badge bg-light text-dark text-wrap">
-                                WiFi
-                            </span>
-                            <span class="badge bg-light text-dark text-wrap">
-                                Television
-                            </span>
-                            <span class="badge bg-light text-dark text-wrap">
-                                AC
-                            </span>
-                            <span class="badge bg-light text-dark text-wrap">
-                                Room heater
+                                4 Children
                             </span>
                         </div>
                         <div class="rating mb-4">
@@ -242,6 +198,77 @@
                             </span>
                             <span class="badge bg-light text-dark text-wrap">
                                 Room heater
+                            </span>
+                        </div>
+                        <div class="guests mb-4">
+                            <h6 class="mb-1">Guests</h6>
+                            <span class="badge bg-light text-dark text-wrap">
+                                5 Adults
+                            </span>
+                            <span class="badge bg-light text-dark text-wrap">
+                                4 Children
+                            </span>
+                        </div>
+                        <div class="rating mb-4">
+                            <h6 class="mb-1">Rating</h6>
+                            <span class="badge rounded-pill bg-light">
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                                <i class="bi bi-star-fill text-warning"></i>
+                            </span>
+                        </div>
+                        <div class="d-flex justify-content-evenly mb-2">
+                            <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
+                            <a href="#" class="btn btn-sm btn-outline-dark shadow-none">More details</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 my-3">
+                <div class="card border-0 shadow" style="max-width: 350px; margin:auto;">
+                    <img src="images/rooms/1.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5>Simple Room Name</h5>
+                        <h6 class="mb-4">20$ per night</h6>
+                        <div class="features mb-4">
+                            <h6 class="mb-1">Features</h6>
+                            <span class="badge bg-light text-dark text-wrap">
+                                2 Rooms
+                            </span>
+                            <span class="badge bg-light text-dark text-wrap">
+                                1 Bathroom
+                            </span>
+                            <span class="badge bg-light text-dark text-wrap">
+                                1 Balcony
+                            </span>
+                            <span class="badge bg-light text-dark text-wrap">
+                                3 Sofa
+                            </span>
+                        </div>
+                        <div class="facilities mb-4">
+                            <h6 class="mb-1">Facilities</h6>
+                            <span class="badge bg-light text-dark text-wrap">
+                                WiFi
+                            </span>
+                            <span class="badge bg-light text-dark text-wrap">
+                                Television
+                            </span>
+                            <span class="badge bg-light text-dark text-wrap">
+                                AC
+                            </span>
+                            <span class="badge bg-light text-dark text-wrap">
+                                Room heater
+                            </span>
+                        </div>
+                        <div class="guests mb-4">
+                            <h6 class="mb-1">Guests</h6>
+                            <span class="badge bg-light text-dark text-wrap">
+                                5 Adults
+                            </span>
+                            <span class="badge bg-light text-dark text-wrap">
+                                4 Children
                             </span>
                         </div>
                         <div class="rating mb-4">
@@ -302,7 +329,6 @@
     </div>
 
     <!-- Testimonials -->
-
 
     <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">TESTIMONIALS</h2>
 
